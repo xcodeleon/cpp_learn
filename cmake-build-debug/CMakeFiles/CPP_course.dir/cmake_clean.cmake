dd878887af9357bd020c91908fcba438272fa597
xcodeleon <49335943+xcodeleon@users.noreply.github.com>
2020-09-24 16:26:00 +0300

    mmm

---
+++ cmake-build-debug/CMakeFiles/CPP_course.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_course.dir/main.cpp.obj"
+  "CPP_course.exe"
+  "CPP_course.exe.manifest"
+  "CPP_course.pdb"
+  "libCPP_course.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_course.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
