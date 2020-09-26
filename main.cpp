418d6baf2cd913953998c1647f9529d54a323fb0
Eugene <xcodeleon@gmail.com>
2020-09-26 13:57:07 +0300

    Functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
+void simon(int);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    simon(3);
+    cout << "Pick an integer: ";
+    int count;
+    cin >> count;
+    simon(count);
+    cout << "Done!" << endl;
     return 0;
+
+}
+
+void simon(int n)
+{
+    cout << "Simon says touch your toys " << n << " times" << endl;
 }
